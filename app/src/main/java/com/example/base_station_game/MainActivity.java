9988f22394a3bb6db76981f7cfdcc7368b55a234
Niclas <niclas.joswig@helsinki.fi>
2019-03-15 17:04:37 +0200

    first parts of gps permission, ui and location manager implementation

--- app/src/main/java/com/example/base_station_game/MainActivity.java
+++ app/src/main/java/com/example/base_station_game/MainActivity.java
@@ -1,38 +1,108 @@
 package com.example.base_station_game;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tv1;
     TextView tv2;
-    int counter=0;
+    int counter = 0;
     Handler handler = new Handler();
     int millisecondsOfDelay = 1000;
     int timesToReapeat = 100;
 
+
+    LocationManager locationManager;
+
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv1 = (TextView)findViewById(R.id.textViewUP);
-        tv2 = (TextView)findViewById(R.id.textViewDOWN);
+        tv1 = (TextView) findViewById(R.id.textViewUP);
+        tv2 = (TextView) findViewById(R.id.textViewDOWN);
 
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 counter++;
-                tv1.setText(""+counter);
-                tv2.setText(""+counter);
-                if(counter<timesToReapeat) {
+                tv1.setText("" + counter);
+                tv2.setText("" + counter);
+                if (counter < timesToReapeat) {
                     handler.postDelayed(this, millisecondsOfDelay);
                 }
             }
         }, millisecondsOfDelay);
+
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                makeUseOfNewLocation(location);
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            public void onProviderEnabled(String provider) {
+            }
+
+            public void onProviderDisabled(String provider) {
+            }
+        };
+
+
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+         }
+
+    private void makeUseOfNewLocation(Location location) {
     }
+
+    private boolean isLocationEnabled() {
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+    }
+
+    private void showAlert() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Enable Location")
+                .setMessage("Your Locations Settings is set to 'Off'.\nPlease Enable Location to " +
+                        "use this app")
+                .setPositiveButton("Location Settings", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                        Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(myIntent);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    }
+                });
+        dialog.show();
+    }
+
 
     @Override
     protected void onStart() {
