e7bcc9cd789ae61d79ee01cd72546db44a0e4684
aboffa <a.boffa95@gmail.com>
2019-03-18 13:02:28 +0200

    Adding coordinates + New UI

--- app/src/main/java/com/example/base_station_game/MainActivity.java
+++ app/src/main/java/com/example/base_station_game/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.base_station_game;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.util.Log;
 
@@ -12,14 +22,14 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG =
         MainActivity.class.getSimpleName();
-    TextView tv1;
-    TextView tv2;
-    int counter=0;
-    Handler handler = new Handler();
-    int millisecondsOfDelay = 1000;
-    int timesToReapeat = 100;
+    //TextView tv1;
+    //TextView tv2;
+    //int counter=0;
+    //Handler handler = new Handler();
+    //int millisecondsOfDelay = 1000;
+    // timesToReapeat = 100;
 
-    @Override
+    /*
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -27,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
         tv2 = (TextView)findViewById(R.id.textViewDOWN);
 
         handler.postDelayed(new Runnable() {
-            @Override
             public void run() {
                 counter++;
                 tv1.setText(""+counter);
@@ -38,12 +47,74 @@ public class MainActivity extends AppCompatActivity {
             }
         }, millisecondsOfDelay);
     }
+    */
+    private Button b;
+    private TextView t;
+    private LocationManager locationManager;
+    private LocationListener listener;
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        //tv1.setText("Hello1");
-        //tv2.setText("Hello2");
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        t = (TextView) findViewById(R.id.textView);
+        b = (Button) findViewById(R.id.button);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+
+        listener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                t.append("\n " + location.getLongitude() + " " + location.getLatitude());
+            }
+
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            public void onProviderEnabled(String s) {
+
+            }
+
+            public void onProviderDisabled(String s) {
+
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
+
+    }
+
+
+    public void sendMessage(View view) {
+        //check weather gps is enabled
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
+                        ,10);
+            }
+            t.setText("error");
+            return;
+        }
+
+        switch (view.getId())
+        {
+            case R.id.button: {t.setText("asdfghjkl");  locationManager.requestLocationUpdates("gps", 5000, 0, listener); break;}
+            case R.id.reset: t.setText(""); break;
+            case R.id.stop_gps:  {t.append("GPS STOPPED");  onPause(); break;}
+        }
+    }
+
+    //stop gps
+    protected void onPause() {
+        super.onPause();
+        //check gps permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.removeUpdates(listener);
     }
 
 
