d3ade8662950376e872cb34a2bc253988a31a6c4
Niclas <niclas.joswig@helsinki.fi>
2019-03-18 13:56:01 +0200

    gps locating on the map working, but no marker yet

--- app/src/main/java/com/example/base_station_game/SecondActivity.java
+++ app/src/main/java/com/example/base_station_game/SecondActivity.java
@@ -1,16 +1,37 @@
 package com.example.base_station_game;
 
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.config.Configuration;
+
+
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.preference.PreferenceManager;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.util.Log;
+import org.osmdroid.views.overlay.Marker;
 
 public class SecondActivity extends AppCompatActivity {
 
 
+    private LocationManager locationManager;
+    private LocationListener listener;
     MapView map = null;
 
     public void onCreate(Bundle savedInstanceState) {
@@ -34,6 +55,49 @@ public class SecondActivity extends AppCompatActivity {
         map.setTileSource(TileSourceFactory.MAPNIK);
         map.setBuiltInZoomControls(true);
         map.setMultiTouchControls(true);
+
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+
+        listener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                //t.append("\n " + location.getLongitude() + " " + location.getLatitude());
+                //GeoPointWrapper wrapper=new GeoPointWrapper();
+                Marker startMarker = new Marker(map);
+                map.getController().setCenter(new GeoPoint(location));
+                startMarker.setPosition(new GeoPoint(location.getLongitude(),location.getLatitude()));
+                startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+                map.getOverlays().add(startMarker);
+            }
+
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            public void onProviderEnabled(String s) {
+
+            }
+
+            public void onProviderDisabled(String s) {
+
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
+    }
+
+    public void sendMessage(View view) {
+        //check weather gps is enabled
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
+                        ,10);
+            }
+            return;
+        }
+
+        locationManager.requestLocationUpdates("gps", 5000, 0, listener);
     }
 
     public void onResume(){
@@ -52,5 +116,9 @@ public class SecondActivity extends AppCompatActivity {
         //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         //Configuration.getInstance().save(this, prefs);
         map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.removeUpdates(listener);
     }
 }
