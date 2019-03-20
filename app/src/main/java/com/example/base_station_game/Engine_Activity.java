bec0a7919e0ffb03e877180b8a66050e82a4f4b6
Niclas <niclas.joswig@helsinki.fi>
2019-03-20 12:05:55 +0200

    writing users to database possible

--- app/src/main/java/com/example/base_station_game/Engine_Activity.java
+++ app/src/main/java/com/example/base_station_game/Engine_Activity.java
@@ -1,32 +1,21 @@
 package com.example.base_station_game;
 
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.IgnoreExtraProperties;
+import com.google.firebase.database.ValueEventListener;
+import com.example.base_station_game.User;
 //import com.google.firebase
 
 public class Engine_Activity extends AppCompatActivity {
 
-    @IgnoreExtraProperties
-    public class User {
-
-        public String username;
-        public String email;
-
-        public User() {
-            // Default constructor required for calls to DataSnapshot.getValue(User.class)
-        }
-
-        public User(String username, String email) {
-            this.username = username;
-            this.email = email;
-        }
-
-    }
 
     private DatabaseReference mDatabase;
 
@@ -37,17 +26,17 @@ public class Engine_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_engine_);
         // Write a message to the database
 
-
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        writeNewUser("ernst","fotze","mail");
+
+        writeNewUser("111","name","mail");
+
     }
 
     private void writeNewUser(String userId, String name, String email) {
         User user = new User(name, email);
-
-        Log.d("first",mDatabase.getRoot().toString());
-        Log.d("second",mDatabase.getDatabase().toString());
-        Log.d("third",mDatabase.setValue("lol").toString());
-        //User user2 = new User(name, email);
+        mDatabase.child("users").child(userId).setValue(user);
     }
+
+
 }
---
+++ app/src/main/java/com/example/base_station_game/User.java
@@ -0,0 +1,15 @@
+package com.example.base_station_game;
+
+public class User {
+    public String username;
+    public String email;
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public User() {
+
+    }
+}
