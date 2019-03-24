5319aa2ebdc4c0145025b80d87c94271ad62f36b
Niclas <niclas.joswig@helsinki.fi>
2019-03-24 12:43:39 +0200

    authentification implemented

--- app/src/main/java/com/example/base_station_game/Engine_Activity.java
+++ app/src/main/java/com/example/base_station_game/Engine_Activity.java
@@ -1,5 +1,6 @@
 package com.example.base_station_game;
 
+import android.content.Intent;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +9,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -18,6 +23,8 @@ import com.google.firebase.database.ValueEventListener;
 import com.example.base_station_game.BaseStation;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class Engine_Activity extends AppCompatActivity {
@@ -30,6 +37,7 @@ public class Engine_Activity extends AppCompatActivity {
     private TextView t_longitude;
     private TextView t_output;
     private ArrayList stations;
+    private static final int RC_SIGN_IN = 123;
 
 
     @Override
@@ -57,26 +65,40 @@ public class Engine_Activity extends AppCompatActivity {
 
        //get_stations();
        update_stations();
+
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build());
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+
     }
 
-    /*public void get_stations(){
-        mDatabase.child("stations").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                t5.setText("");
-                for (DataSnapshot ds : dataSnapshot.getChildren()) {
-                    String id = ds.getKey();
-                    BaseStation station = ds.getValue(BaseStation.class);
-                    t5.append(station.toString()+"\n");
-                }
-            }
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                Log.d("error message:","The read failed: " + databaseError.getCode());
-            }
-        });
 
-    }*/
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
 
     public void update_stations(){
         mDatabase.child("stations").addChildEventListener(new ChildEventListener() {
@@ -137,4 +159,23 @@ public class Engine_Activity extends AppCompatActivity {
         deleteBaseStationFromDatabase(id,name,latitude,longitude);
     }
 
+
+    /*public void get_stations(){
+        mDatabase.child("stations").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                t5.setText("");
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    String id = ds.getKey();
+                    BaseStation station = ds.getValue(BaseStation.class);
+                    t5.append(station.toString()+"\n");
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d("error message:","The read failed: " + databaseError.getCode());
+            }
+        });
+
+    }*/
 }
