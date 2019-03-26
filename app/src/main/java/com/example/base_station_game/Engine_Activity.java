b042e8e014f01124a6588045a3b47fac13d8cdeb
Niclas <niclas.joswig@helsinki.fi>
2019-03-26 19:52:13 +0200

    working service implementation

--- app/src/main/java/com/example/base_station_game/Engine_Activity.java
+++ app/src/main/java/com/example/base_station_game/Engine_Activity.java
@@ -1,15 +1,11 @@
 package com.example.base_station_game;
 
 import android.content.Intent;
-import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -18,16 +14,9 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.IgnoreExtraProperties;
 import com.google.firebase.database.ValueEventListener;
-import com.example.base_station_game.BaseStation;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import com.example.base_station_game.User;
 
 public class Engine_Activity extends AppCompatActivity {
 
@@ -71,7 +60,7 @@ public class Engine_Activity extends AppCompatActivity {
        //get_stations();
        update_stations();
 
-        List<AuthUI.IdpConfig> providers = Arrays.asList(
+   /*     List<AuthUI.IdpConfig> providers = Arrays.asList(
                 new AuthUI.IdpConfig.EmailBuilder().build());
 
         // Create and launch sign-in intent
@@ -81,8 +70,13 @@ public class Engine_Activity extends AppCompatActivity {
                         .setAvailableProviders(providers)
                         .build(),
                 RC_SIGN_IN);
+*/
 
+    }
 
+    public void runservice(View view){
+        Intent intent = new Intent(this,  ServiceActivity.class);
+        startActivity(intent);
     }
 
     private void load_or_create_user(final FirebaseUser firebaseUser){
