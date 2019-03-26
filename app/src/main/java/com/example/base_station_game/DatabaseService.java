c0efd743a4a6e701089b55f263c8a5b25cfc3266
Niclas <niclas.joswig@helsinki.fi>
2019-03-27 00:44:33 +0200

    service used to get database entries

--- app/src/main/java/com/example/base_station_game/DatabaseService.java
+++ app/src/main/java/com/example/base_station_game/DatabaseService.java
@@ -7,6 +7,13 @@ import android.os.Binder;
 import android.os.IBinder;
 import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 import java.util.Random;
 
 
@@ -16,8 +23,14 @@ public class DatabaseService extends IntentService {
     // Random number generator
     private final Random mGenerator = new Random();
 
+    private DatabaseReference mDatabase;
+    private ArrayList stations;
+
     public DatabaseService() {
         super("DatabaseService2");
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        stations = new ArrayList();
+        update_stations();
     }
 
     public class LocalBinder extends Binder {
@@ -27,6 +40,9 @@ public class DatabaseService extends IntentService {
         }
     }
 
+    public ArrayList getStations(){
+        return stations;
+    }
     @Override
     protected void onHandleIntent(Intent intent) {
         // Normally we would do some work here, like download a file.
@@ -62,4 +78,34 @@ public class DatabaseService extends IntentService {
     }
 
 
+    public void update_stations() {
+        mDatabase.child("stations").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot ds, String prevChildKey) {
+                BaseStation station = ds.getValue(BaseStation.class);  //get station object
+                stations.add(station);                                 //add station object to list of station
+                //t_output.setText(stations.toString());                       //update ui
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String prevChildKey) {
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot ds) {
+                BaseStation station = ds.getValue(BaseStation.class);    //get station object
+                stations.remove(station);                                //remove station object to list of station
+                //t_output.setText(stations.toString());                         //update ui
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String prevChildKey) {
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+
+    }
 }
--- app/src/main/java/com/example/base_station_game/ServiceActivity.java
+++ app/src/main/java/com/example/base_station_game/ServiceActivity.java
@@ -5,19 +5,32 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class ServiceActivity extends AppCompatActivity {
 
     DatabaseService mService;
     boolean mBound = false;
     private TextView text;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +57,7 @@ public class ServiceActivity extends AppCompatActivity {
         }
     };
 
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -56,8 +70,8 @@ public class ServiceActivity extends AppCompatActivity {
 
         //Toast.makeText(this, "number", Toast.LENGTH_SHORT).show();
 
-        int a=mService.getRandomNumber();
-        text.setText(Integer.toString(a));
+        ArrayList a=mService.getStations();
+        text.setText(a.toString());
 
     }
 
