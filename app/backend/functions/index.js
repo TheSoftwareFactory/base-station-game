2c919daad3db7415db86473062ebad59fa2aa283
Niclas <niclas.joswig@helsinki.fi>
2019-03-30 00:28:31 +0200

    backend functions for adding and deleting stations

--- app/backend/functions/index.js
+++ app/backend/functions/index.js
@@ -7,7 +7,39 @@ admin.initializeApp();
 
 // Take the text parameter passed to this HTTP endpoint and insert it into the
 // Realtime Database under the path /messages/:pushId/original
-exports.addMessage = functions.https.onRequest((req, res) => {
+exports.addStation = functions.https.onRequest((req, res) => {
+  // Grab the text parameter.
+  var original = req.query.text;
+  var a=original.split(",");
+  console.log(a);
+  console.log(a[0]);
+  // Push the new message into the Realtime Database using the Firebase Admin SDK.
+
+  return admin.database().ref('/stations').push({"name":a[0],"latitude":a[1],"longitude":a[2],"timeToLive":a[3]}).then((snapshot) => {
+    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+    return res.redirect(302, snapshot.ref.toString());
+  });
+});
+
+exports.deleteStation = functions.https.onRequest((req, res) => {
+  const original = req.query.text;
+  return admin.database().ref('/stations').child(original).remove().then((snapshot) => {
+    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+    return res.redirect(302, snapshot.ref.toString());
+  });
+});
+
+exports.deleteAllStations = functions.https.onRequest((req, res) => {
+
+  return admin.database().ref('/stations').remove().then((snapshot) => {
+    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+    return res.redirect(302, snapshot.ref.toString());
+  });
+});
+
+// Take the text parameter passed to this HTTP endpoint and insert it into the
+// Realtime Database under the path /messages/:pushId/original
+/*exports.addMessage = functions.https.onRequest((req, res) => {
   // Grab the text parameter.
   const original = req.query.text;
   // Push the new message into the Realtime Database using the Firebase Admin SDK.
@@ -15,18 +47,4 @@ exports.addMessage = functions.https.onRequest((req, res) => {
     // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
     return res.redirect(303, snapshot.ref.toString());
   });
-});
-
-// Listens for new messages added to /messages/:pushId/original and creates an
-// uppercase version of the message to /messages/:pushId/uppercase
-/*exports.makeUppercase = functions.database.ref('/stations/{id}/name')
-    .onCreate((snapshot, context) => {
-      // Grab the current value of what was written to the Realtime Database.
-      const original = snapshot.val();
-      console.log('Uppercasing', context.params.pushId, original);
-      const uppercase = original.toUpperCase();
-      // You must return a Promise when performing asynchronous tasks inside a Functions such as
-      // writing to the Firebase Realtime Database.
-      // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-      return snapshot.ref.parent.child('name').set(uppercase);
-    });*/
+});*/
