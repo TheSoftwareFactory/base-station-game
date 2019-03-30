aa4a52cc4a028016baac79498578b21c7502513e
xlDownxl <niclas.joswig@helsinki.fi>
2019-03-30 18:20:50 +0200

    first sketch of finished minigame database interaction

--- app/src/main/java/com/example/base_station_game/Engine_Activity.java
+++ app/src/main/java/com/example/base_station_game/Engine_Activity.java
@@ -93,10 +93,24 @@ public class Engine_Activity extends AppCompatActivity {
         });
     }
 
-
     public void login(View view){
-        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        t_output.setText(firebaseUser.toString());
+        BaseStation station=(BaseStation) stations.get(0);
+        int score=14;
+        conquered(station,score);
+    }
+
+    // function for minigame activity: pushes score to base station tag in database
+    public void conquered(BaseStation station,double score){
+
+        if (user.getTeam()==1){
+            mDatabase.child("stations").child(station.getID()).child("BlueConquerer").child(user.getUID()).setValue(score);
+        }
+        else
+        {
+            mDatabase.child("stations").child(station.getID()).child("RedConquerer").child(user.getUID()).setValue(score);
+        }
+
+        mDatabase.child("Users").child(user.getUID()).child("conqueredStations").child(station.getID()).setValue(score);
     }
 
     @Override
