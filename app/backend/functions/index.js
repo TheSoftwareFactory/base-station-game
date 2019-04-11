ea7b6c6e68c782ecdab8870e54c14e4f79e89a88
Niclas <niclas.joswig@helsinki.fi>
2019-04-12 00:34:58 +0300

    clean up of backend functions

--- app/backend/functions/index.js
+++ app/backend/functions/index.js
@@ -5,24 +5,6 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-/*
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
-exports.addStation = functions.https.onRequest((req, res) => {
-  // Grab the text parameter.
-  var original = req.query.text;
-  var a=original.split(",");
-  console.log(a);
-  console.log(a[0]);
-  // Push the new message into the Realtime Database using the Firebase Admin SDK.
-
-  return admin.database().ref('/stations').push({"name":a[0],"latitude":a[1],"longitude":a[2],"timeToLive":a[3]}).then((snapshot) => {
-    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-    return res.redirect(302, snapshot.ref.toString());
-  });
-});
-*/
-
 exports.deleteStation = functions.https.onRequest((req, res) => {
   const original = req.query.text;
   return admin.database().ref('/stations').child(original).remove().then((snapshot) => {
@@ -57,68 +39,12 @@ exports.addStation = functions.https.onRequest((req, res) => {
   })
 });
 
-exports.stationDies = functions.database.ref('/stations/{stationId}/timeToLive')
-  //StationId is going to die
-  .onUpdate((change, context) => {
-    const stationExp = 500;
-    // Grab the current value of what was written to the Realtime Database.
-    const newValue = change.after.val();
-    var id = context.params.stationId;
-    console.log('Time to live of station', id, newValue);
-
-    //if station dies
-    if (newValue > new Date()) {
-      console.log("station dies");
-
-      //get snapshot of stations key
-      var starCountRef = admin.database().ref('stations');
-      starCountRef.once('value').then(function (snapshot) {
-        //get all stations as snapshot
-        snapshot.forEach((childSnapshot) => {
-          console.log(childSnapshot.val());
-
-          //get the conquerer list of each station
-          childSnapshot.child("BlueConquerer").forEach(function (babySnapshot) {
-            var key = babySnapshot.key;
-            var val = babySnapshot.val();
-            console.log("erster conquerer: ")
-            console.log(key);
-            console.log(val);
-
-
-            admin.database().ref('Users').child(key).once("value", xd => {
-              if (xd.exists()) {
-                //const userData = xd.val();
-                //console.log("exists!", userData);
-
-                //update exp based on old exp
-                admin.database().ref('Users').child(key).child("exp").once('value', function (conquerer) {
-                  admin.database().ref('Users').child(key).child("exp").set(conquerer.val() + stationExp);
-                });
-                //update count of conquered base stations for user
-                admin.database().ref('Users').child(key).child("ConqueredStations").once('value', function (conquerer) {
-                  admin.database().ref('Users').child(key).child("ConqueredStations").set(conquerer.val() + 1);
-                });
-              }
-            });
-          });
-
-        });
-        //todo: delete station
-        //admin.database().ref('stations').remove(id);
-      return res.send("durchgelaufen")
-      }).catch(function(error){
-        console.log(error)
-        res.send(error)
-      });
-    }
-  });
 
 exports.winningteam_update = functions.database.ref('stations/{stationId}/Teams/{teamId}/teamScore')
   .onUpdate((change, context) => {
     var winningteam="";
     var winningscore=0;
-    admin.database().ref("stations").child(context.params.stationId).child("Teams").once('value').then(function(teams){
+    return admin.database().ref("stations").child(context.params.stationId).child("Teams").once('value').then(function(teams){
         teams.forEach(function(team){
           //console.log("team: ",team.val());
           console.log("teamscore",team.child("teamScore").val());
@@ -135,14 +61,14 @@ exports.winningteam_update = functions.database.ref('stations/{stationId}/Teams/
         console.log(error)
         res.send(error)
       });
-    return "nice"
+   
   })
 
   exports.winningteam_create = functions.database.ref('stations/{stationId}/Teams/{teamId}/teamScore')
   .onCreate((change, context) => {
     var winningteam="";
     var winningscore=0;
-    admin.database().ref("stations").child(context.params.stationId).child("Teams").once('value').then(function(teams){
+    return admin.database().ref("stations").child(context.params.stationId).child("Teams").once('value').then(function(teams){
         teams.forEach(function(team){
           //console.log("team: ",team.val());
           console.log("teamscore",team.child("teamScore").val());
@@ -159,102 +85,7 @@ exports.winningteam_update = functions.database.ref('stations/{stationId}/Teams/
         console.log(error)
         res.send(error)
       });
-    return "nice"
   })
-
-//var List = require("collections/list");
-
-exports.stationDiesChrono = functions.https.onRequest((req, res) => {
-  //StationId is going to die
-  //get snapshot of stations key
-  var deadStations="";
-  var starCountRef = admin.database().ref('stations');
-  starCountRef.once('value').then(function (snapshot) {
-    //get all stations as snapshot
-    snapshot.forEach(function (childSnapshot) {
-
-      
-      var winningteam="";
-      var winningscore=0;
-      var stationkey=childSnapshot.key;
-      console.log("station key: ",childSnapshot.key);
-      console.log("station val: ",childSnapshot.val());
-
-
-      // when Teams empty -> stop doing anything
-      admin.database().ref("stations").child(childSnapshot.key).child("winnerTeam").once('value', function(snap){
-        console.log("winningref:",snap.ref.toString());
-        //console.log("winningteam:",snap.val());
-        winningteam=snap.val();     
-        console.log("winningteam start: ",winningteam)    
-      })
-      
-      //Check for right condition
-      //console.log(childSnapshot.val()['name'],"<------- station");
-      //console.log(childSnapshot.val(),"<------- station");
-      //console.log(childSnapshot.child('timeToLive').val())
-      //console.log(new Date());
-      if (new Date(childSnapshot.child("timeToLive").val()) > new Date()) {
-        console.log("station dies");
-        //get the conquerer list of each station
-        //console.log("teamübersicht",childSnapshot.child("Teams").val());
-      
-        childSnapshot.child("Teams").forEach(function (teamSnapshot) {
-          //var key = teamSnapshot.key;
-          var teamname=teamSnapshot.key;
-          /*console.log("team snapshot, team:",teamSnapshot.val());
-            
-          //console.log("teamscore: ",lol.child("teamScore").val()); 
-          console.log("interessante infos:")    */
-          //console.log("winningteam: ",winningteam)
-          console.log("currentteam: ",teamname) 
-          if(teamname===winningteam){
-
-            //console.log(val);
-            teamSnapshot.child("Players").forEach(function (userScore) {
-              //console.log(userScore.key);
-              //console.log(userScore.val());
-              var key = userScore.key;
-              var val = userScore.val();
-
-              admin.database().ref('Users').child(key).once("value", xd => {
-                if (xd.exists()) {
-                  //var userData = xd.val();
-                  //console.log("user iteration", userData);
-
-                  //console.log("score",val);
-                  //update exp based on old exp
-                  admin.database().ref('Users').child(key).child("exp").once('value', function (conquerer) {
-                    //console.log("previousexp",conquerer.val());
-                    admin.database().ref('Users').child(key).child("exp").set(conquerer.val() + val);
-                      /* admin.database().ref('Users').child(key).child("exp").once('value', function (lol) {
-                        console.log("exp after",lol.val());
-                      }); */
-                      //return "done";
-                  });
-                
-                  //update count of conquered base stations for user
-                  admin.database().ref('Users').child(key).child("ConqueredStations").once('value', function (conquerer) {
-                    admin.database().ref('Users').child(key).child("ConqueredStations").child(childSnapshot.val()['name']).set(val);
-                  });
-                }
-              });
-            })
-          }
-        });
-        deadStations=deadStations+stationkey.toString();
-      }
-      //todo: delete station
-      //admin.database().ref('stations').remove(id);
-    });
-    
-  return res.send(deadStations)
-  }).catch(function(error){
-    console.log("error: ",error)
-    res.send(error)
-  });
-  return "nice"
-});
 
 
 exports.updateTeamScores = functions.database.ref('stations/{stationId}/Teams/{teamId}/Players')
@@ -277,8 +108,7 @@ exports.updateTeamScores = functions.database.ref('stations/{stationId}/Teams/{t
       teamscore = teamscore + snapshot.val();
     });
     console.log("teamscore: ",teamscore)
-    admin.database().ref('stations').child(station_id).child("Teams").child(team_id).child("teamScore").set(teamscore);
-    return "nice";
+    return admin.database().ref('stations').child(station_id).child("Teams").child(team_id).child("teamScore").set(teamscore);    
   }
   });
 
@@ -375,13 +205,8 @@ exports.playedStationsScoreToExp = functions.database.ref('Users/{userId}/Played
 // exports.playedStationsScoreToExp = functions.database.ref('Users/{userId}/ConqueredStations')
 //   .onCreate(scoreToExp);
 
-exports.dummy = functions.https.onRequest((req, res) => {
-  return res.send("Hello");
-})
 
-exports.stationDiesChronoDebug2 = functions.https.onRequest((req, res) => {
-  //StationId is going to die
-  //get snapshot of stations key
+exports.stationDiesChrono = functions.https.onRequest((req, res) => {
   var deadStations=[];
   const promises=[];
   
@@ -393,71 +218,65 @@ exports.stationDiesChronoDebug2 = functions.https.onRequest((req, res) => {
       var winningteam="";
       var winningscore=0;
       var stationkey=childSnapshot.key;
-      console.log("station key: ",childSnapshot.key);
+      //console.log("station key: ",childSnapshot.key);
 
       p = admin.database().ref("stations").child(childSnapshot.key).child("winnerTeam").once('value').then(snap=>{
         winningteam=snap.val(); 
 
-        console.log("this is station: ",stationkey,"and my winner team is",winningteam)       
+        //console.log("this is station: ",stationkey,"and my winner team is",winningteam)       
         //Check for right condition
         //console.log(childSnapshot.val()['name'],"<------- station");  
         //console.log("timetolive",childSnapshot.child('timeToLive').val())
         //console.log("new date",new Date());
 
         if (new Date(childSnapshot.child("timeToLive").val()) < new Date()) {
-          console.log("station dies",stationkey);
+          //console.log("station dies",stationkey);
           deadStations.push(stationkey)
 
           const inner_promises=[];
           childSnapshot.child("Teams").child(winningteam).child("Players").forEach(function (userScore) {
-            console.log("player score",userScore.val())
-            console.log("player id",userScore.key)
+            //console.log("player score",userScore.val())
+            //console.log("player id",userScore.key)
 
             var key = userScore.key;
             var val = userScore.val();
             
             const final_promises=[];
-            inside=admin.database().ref('Users').child(key).once("value", xd => {
-              if (xd.exists()) {
-                var userData = xd.val();
-                console.log("user iteration", userData);
+            inside=admin.database().ref('Users').child(key).once("value", user => {
+              if (user.exists()) {
+                //var userData = user.val();
+                //console.log("user iteration", userData);
 
                 a=admin.database().ref('Users').child(key).child("exp").once('value').then(conquerer=> {
-                  console.log("previousexp",conquerer.val(),"of user ",key);
+                  //console.log("previousexp",conquerer.val(),"of user ",key);
                  
-                  return admin.database().ref('Users').child(key).child("exp").set(conquerer.val() + 500).then(gf=>{
-                    return true;
-                  });              
+                  return admin.database().ref('Users').child(key).child("exp").set(conquerer.val() + 500);
                 });
                
                 b=admin.database().ref('Users').child(key).child("ConqueredStations").once('value').then(conquerer => {
-                  return admin.database().ref('Users').child(key).child("ConqueredStations").child(childSnapshot.val()['name']).set(val).then(rr=>{
-                    return true;
-                  });              
+                  return admin.database().ref('Users').child(key).child("ConqueredStations").child(childSnapshot.val()['name']).set(val);
                 });
                 final_promises.push(a)
                 final_promises.push(b)
                 
               }
-              console.log("final promises",final_promises)
+              //console.log("final promises",final_promises)
               return Promise.all(final_promises)
-            }).then(eded=>{
-               return inner_promises.push(inside) 
             })
+            inner_promises.push(inside)           
                       
           })    
           return Promise.all(inner_promises);             
         }
           else {
-            console.log("im else teile")
+            //console.log("station is alive")
             return true}         
       })
       promises.push(p);      
     })
     return Promise.all(promises)
   }).then(kek =>{
-    console.log("durch")
-    return res.send(deadStations.toString()+" sind gestorben")
+    return res.send("stations "+deadStations.toString()+"  died")
   })
   .catch(error=>{
     console.log("error: ",error)
