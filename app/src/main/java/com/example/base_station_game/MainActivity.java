104d1fd4db962a146227c44c8e9c769a92417bd7
Niclas <niclas.joswig@helsinki.fi>
2019-04-19 09:56:48 +0300

    finished authentification rework

--- app/src/main/java/com/example/base_station_game/MainActivity.java
+++ app/src/main/java/com/example/base_station_game/MainActivity.java
@@ -45,147 +45,148 @@ public class MainActivity extends AppCompatActivity {
     private static final int RC_SIGN_IN = 123;
     private DatabaseReference mDatabase;
     public User user;
-    private TextView email;
-    private TextView username;
-    private TextView password;
-    private TextView team;
+    private TextView email_field;
+    private TextView username_field;
+    private TextView password_field;
+    private TextView team_field;
 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //login();
-        //load_or_create_user();
+
         setContentView(R.layout.activity_main);
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        //mDatabase.child("stations").setValue("");
-        //logout();
     }
 
-    private void login() {
-        List<AuthUI.IdpConfig> providers = Arrays.asList(
-                new AuthUI.IdpConfig.EmailBuilder().build());
+   public void logout(View v){
+       FirebaseAuth.getInstance().signOut();
+       user=null;
+   }
 
-        // Create and launch sign-in intent
-        startActivityForResult(
-                AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setAvailableProviders(providers)
-                        .build(),
-                RC_SIGN_IN);
+   public void register(View v) {
+       email_field = (TextView) findViewById(R.id.email);
+       String email=email_field.getText().toString();
+       team_field = (TextView) findViewById(R.id.team);
+       String team=team_field.getText().toString();
+       username_field = (TextView) findViewById(R.id.username);
+       String username=username_field.getText().toString();
+       password_field = (TextView) findViewById(R.id.password);
+       String password=password_field.getText().toString();
 
-    }
+       if ( password.equals("") || email.equals("") || team.equals("") || username.equals(""))
+       {
+           Toast.makeText(MainActivity.this, "Please fill all fields!",
+                   Toast.LENGTH_SHORT).show();
+       }
+       else {
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // Check which request we're responding to
-        if (requestCode == RC_SIGN_IN) {
-            // Make sure the request was successful
-            //if (resultCode == RESULT_OK) {
-            // The user picked a contact.
-            // The Intent's data Uri identifies which contact was selected.
+           FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                   .addOnCompleteListener(MainActivity.this, task -> {
+                       if (task.isSuccessful()) {
+                           Toast.makeText(MainActivity.this, username + " registered!",
+                                   Toast.LENGTH_SHORT).show();
+                           mDatabase = FirebaseDatabase.getInstance().getReference();
+                           FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
-            // Do something with the contact here (bigger example below)
-            //}
-            load_or_create_user();
-        }
-    }
+                           user = new User(firebaseUser.getUid(), firebaseUser.getEmail(), username, team);
+                           user.beUpdated();
+                           mDatabase.child("Users").child(user.getUID()).child("username").setValue(user.getUsername());
+                           mDatabase.child("Users").child(user.getUID()).child("email").setValue(user.getEmail());
+                           mDatabase.child("Users").child(user.getUID()).child("uid").setValue(user.getUID());
+                           mDatabase.child("Users").child(user.getUID()).child("team").setValue(user.getTeam());
+                       } else {
+                           Toast.makeText(MainActivity.this, "Registering failed.",
+                                   Toast.LENGTH_SHORT).show();
+                       }
+                   });
+       }
+   }
 
-    private void logout() {
-        FirebaseAuth.getInstance().signOut();
-    }
-
-    private void load_or_create_user() {
-        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
-            mDatabase = FirebaseDatabase.getInstance().getReference();
-            FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-            DatabaseReference ref = mDatabase.child("Users").child(firebaseUser.getUid()); //check at reference of user if it already exists
-            ref.addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    if (dataSnapshot.hasChild("team")) {  //user already exists
-                        user = dataSnapshot.getValue(User.class);
-                        user.beUpdated();
-                    } else {  //create new user
-
-                                            user = new User(firebaseUser.getUid(), firebaseUser.getEmail(), firebaseUser.getDisplayName(),  firebaseUser.);
-                                            user.beUpdated();
-                                            mDatabase.child("Users").child(user.getUID()).child("username").setValue(user.getUsername());
-                                            mDatabase.child("Users").child(user.getUID()).child("email").setValue(user.getEmail());
-                                            mDatabase.child("Users").child(user.getUID()).child("uid").setValue(user.getUID());
-                                            mDatabase.child("Users").child(user.getUID()).child("team").setValue(user.getTeam());
-
-
-                    }
-                }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-                }
-            });
-        }
-    }
-
-
-
-    // -> merge together login and load create user
     public void login(View v) {
-        email = (TextView) findViewById(R.id.email);
-        username = (TextView) findViewById(R.id.username);
-        password = (TextView) findViewById(R.id.password);
-        team = (TextView) findViewById(R.id.team);
-        FirebaseAuth.getInstance().signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
-                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d("TAG", "signInWithEmail:onComplete:" + task.isSuccessful());
-                        load_or_create_user();
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
-                        if (!task.isSuccessful()) {
-                            Log.w("TAG", "signInWithEmail", task.getException());
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+        email_field = (TextView) findViewById(R.id.email);
+        String email = email_field.getText().toString();
+        password_field = (TextView) findViewById(R.id.password);
+        String password = password_field.getText().toString();
 
+        if ( password.equals("") || email.equals(""))
+        {
+            Toast.makeText(MainActivity.this, "Please enter both Email and Password",
+                    Toast.LENGTH_SHORT).show();
+        }
+        else {
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+
+                                mDatabase = FirebaseDatabase.getInstance().getReference();
+                                FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+                                DatabaseReference ref = mDatabase.child("Users").child(firebaseUser.getUid()); //check at reference of user if it already exists
+                                ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(DataSnapshot dataSnapshot) {
+                                        user = dataSnapshot.getValue(User.class);
+                                        user.beUpdated();
+                                        ref.removeEventListener(this);
+                                        Toast.makeText(MainActivity.this, user.getUsername() + " signed in.",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+
+                                    @Override
+                                    public void onCancelled(DatabaseError databaseError) {
+                                    }
+                                });
+
+                            } else {
+                                Log.w("TAG", "signInWithEmail", task.getException());
+                                Toast.makeText(MainActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
     }
 
 
     public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.buttonMAP: {
-                Intent intent = new Intent(this, SecondActivity.class);
-                intent.putExtra("user", user);
-                startActivity(intent);
-                break;
+        if (user == null) {
+            Toast.makeText(MainActivity.this, "Please log in before continuing.",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            switch (v.getId()) {
+                case R.id.buttonMAP: {
+                    Intent intent = new Intent(this, SecondActivity.class);
+                    intent.putExtra("user", user);
+                    startActivity(intent);
+                    break;
+                }
+                case R.id.sample_debug: {
+                    Intent intent = new Intent(this, SamplingDebugActivity.class);
+                    intent.putExtra("user", user);
+                    startActivity(intent);
+                    break;
+                }
+                case R.id.google_engine: {
+                    Intent intent = new Intent(this, Engine_Activity.class);
+                    intent.putExtra("user", user);
+                    startActivity(intent);
+                    break;
+                }
+                case R.id.user_profile: {
+                    Intent intent = new Intent(this, UserProfile.class);
+                    intent.putExtra("user", user);
+                    startActivity(intent);
+                    break;
+                }
+                case R.id.settings: {
+                    Intent intent = new Intent(this, Settings.class);
+                    intent.putExtra("user", user);
+                    startActivity(intent);
+                    break;
+                }
+                default:
+                    throw new RuntimeException("Unknown button ID");
             }
-            case R.id.sample_debug: {
-                Intent intent = new Intent(this, SamplingDebugActivity.class);
-                intent.putExtra("user", user);
-                startActivity(intent);
-                break;
-            }
-            case R.id.google_engine: {
-                Intent intent = new Intent(this, Engine_Activity.class);
-                intent.putExtra("user", user);
-                startActivity(intent);
-                break;
-            }
-            case R.id.user_profile: {
-                Intent intent = new Intent(this, UserProfile.class);
-                intent.putExtra("user", user);
-                startActivity(intent);
-                break;
-            }
-            case R.id.settings: {
-                Intent intent = new Intent(this, Settings.class);
-                intent.putExtra("user", user);
-                startActivity(intent);
-                break;
-            }
-            default:
-                throw new RuntimeException("Unknown button ID");
         }
     }
 }
