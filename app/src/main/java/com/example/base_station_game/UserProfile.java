76251541e401c8907d6463340dc88a06db31f412
Niclas <niclas.joswig@helsinki.fi>
2019-04-28 11:51:39 +0300

    design rework, exp displaying and team displaying

--- app/src/main/java/com/example/base_station_game/UserProfile.java
+++ app/src/main/java/com/example/base_station_game/UserProfile.java
@@ -2,6 +2,7 @@ package com.example.base_station_game;
 
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.os.DeadObjectException;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,6 +22,9 @@ public class UserProfile extends AppCompatActivity {
     TextView exp = null;
     TextView level = null;
     TextView team = null;
+    TextView players = null;
+    long player_count=0;
+    boolean team_registered;
     private DatabaseReference mDatabase;
     User user;
 
@@ -33,7 +37,7 @@ public class UserProfile extends AppCompatActivity {
     @Override
     protected void onStart(){
         super.onStart();
-
+        team_registered=false;
         mDatabase = FirebaseDatabase.getInstance().getReference();
         DatabaseReference ref = mDatabase.child("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
         ref.addValueEventListener(new ValueEventListener() {
@@ -41,6 +45,9 @@ public class UserProfile extends AppCompatActivity {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 user = dataSnapshot.getValue(User.class);
                 updateUI();
+                if (!team_registered){
+                    registerTeam();
+                }
             }
 
             @Override
@@ -48,20 +55,42 @@ public class UserProfile extends AppCompatActivity {
             }
         });
 
+
+
+
         username = findViewById(R.id.username);
         email = findViewById(R.id.email);
         exp = findViewById(R.id.exp);
         level = findViewById(R.id.level);
         team = findViewById(R.id.team);
+        players = findViewById(R.id.players);
 
 
     }
 
+    private void registerTeam(){
+        DatabaseReference ref = mDatabase.child("Teams").child(user.getTeam());
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                player_count = dataSnapshot.getChildrenCount();
+                players.setText("Active Players:  "+player_count);
+                team_registered=true;
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    }
+
     private void updateUI(){
+        Double max_exp=100.0;
         username.setText(user.getUsername());
-        email.setText(user.getEmail());
-        exp.setText("" + user.getExp());
-        level.setText("" + user.getLevel());
+        email.setText("Mail Address:  "+user.getEmail());
+        Double exp_percentage= user.getExp()/max_exp*100;
+        exp.setText("Experience:  " + user.getExp()+"/100  ("+exp_percentage.intValue()+"%)");
+        level.setText("Level " + user.getLevel());
         team.setText("" + user.getTeam());
     }
 }
