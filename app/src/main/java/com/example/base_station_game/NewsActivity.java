a8d4c5d319dd4ee9fcd0c829cfaefdb2070ee28e
Bankowski Victor <bavi@sf-6.cs.helsinki.fi>
2019-04-29 16:44:02 +0300

    added a newsadapter so that we can actually display things in the news activity. the adapter most likely doesn't work yet

--- app/src/main/java/com/example/base_station_game/NewsActivity.java
+++ app/src/main/java/com/example/base_station_game/NewsActivity.java
@@ -2,12 +2,30 @@ package com.example.base_station_game;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class NewsActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+        recyclerView = (RecyclerView) findViewById(R.id.news_feed);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
     }
 }
